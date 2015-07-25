c19e6858d7301947e139555dd327e1359efc9482
Szymon B <ponifacy@wp.pl>
2015-07-25 14:21:51 +0200

    Add content field to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :user
 
-  validates_presence_of :author_id, :user_id, :rating
+  validates_presence_of :author_id, :user_id, :rating, :content
   validates_inclusion_of :number, :in => 1..5
+  validates :conten, length: { maximum: 100 }
 end
