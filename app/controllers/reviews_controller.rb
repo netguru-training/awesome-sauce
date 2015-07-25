b28832590f1da616427464134095f856c7d5e58e
szymonbaranowski <szymon.baranowski.pl@gmail.com>
2015-07-25 14:40:16 +0200

    Add create review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def new
+  
+  end
+
+  def create
+    self.review = Review.new(review_params)
+    self.review.author=current_user
+    if review.save
+      flash[:success] = "Review created."
+      ## redirect_to sciezka usera.....
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:content, :rating, :user_id)
+    end
 end
