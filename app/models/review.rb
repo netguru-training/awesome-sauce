6bd35010e9fcc0b792c742de702a85790c187cc2
Szymon B <ponifacy@wp.pl>
2015-07-25 14:06:51 +0200

    Add reviews validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :user
+
+  validates_presence_of :author_id, :user_id, :rating
+  validates_inclusion_of :number, :in => 1..5
 end
