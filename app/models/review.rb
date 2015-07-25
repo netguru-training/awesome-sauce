34bd811b276eb7677a196cd9aa10057de024793d
szymonbaranowski <szymon.baranowski.pl@gmail.com>
2015-07-25 15:22:35 +0200

    Fix validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :author_id, :user_id, :rating, :content
-  validates_inclusion_of :number, :in => 1..5
-  validates :conten, length: { maximum: 100 }
+  validates :rating, inclusion: { in: 1..5 }
+  validates :content, length: { maximum: 100 }
 end
