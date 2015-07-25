908303baa4a9c271a2e0cf247bc26b0254cacf97
Szymon B <ponifacy@wp.pl>
2015-07-25 14:07:21 +0200

    Add average_rating method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
   has_many :reviews_given, class_name: "Review", :foreign_key => "author_id"
   has_many :reviews_received, class_name: "Review", :foreign_key => "user_id"
 
+  def average_rating
+    if reviews_received.any?
+      reviews_received.average(:rating).round(2)
+    else
+      "n/a"
+    end
+  end
+
   def set_default_role
     self.role ||= :user
   end
