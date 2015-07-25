025fb6861ced6ff5b8e034fa2059a13b2868304e
szymonbaranowski <szymon.baranowski.pl@gmail.com>
2015-07-25 15:19:20 +0200

    Add decent_exposure config for strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  decent_configuration do
+    strategy DecentExposure::StrongParametersStrategy
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
