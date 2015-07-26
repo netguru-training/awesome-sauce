6f67e220f18c620f8390d90e103152bfa2dac591
WojciechKo <korzeniowski.wojciech@gmail.com>
2015-07-26 09:22:23 +0200

    Shut the hound down vol. 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,48 +9,48 @@
 admin = CreateAdminService.new.call
 users = 5.times.collect do |i|
   User.create(
-      email: "test#{i}@a.com",
-      name: "test#{i}",
-      password: 'asdasdasd',
-      password_confirmation: 'asdasdasd')
+        email: "test#{i}@a.com",
+        name: "test#{i}",
+        password: 'asdasdasd',
+        password_confirmation: 'asdasdasd')
 end
 
 ride = Ride.create(
-    start_city: 'Tomaszów',
-    destination_city: 'Warszawa',
-    seats: 5,
-    start_date: Time.now + 2.days,
-    driver: users.at(1))
+      start_city: 'Tomaszów',
+      destination_city: 'Warszawa',
+      seats: 5,
+      start_date: Time.now + 2.days,
+      driver: users.at(1))
 
 RidesPassenger.create(passenger: users.at(2), ride: ride)
 RidesPassenger.create(passenger: users.at(3), ride: ride)
 RidesPassenger.create(passenger: users.at(4), ride: ride)
 
 ride = Ride.create(
-    start_city: 'Warszawa',
-    destination_city: 'Gdańsk',
-    seats: 5,
-    start_date: Time.now + 3.days,
-    driver: users.at(0))
+      start_city: 'Warszawa',
+      destination_city: 'Gdańsk',
+      seats: 5,
+      start_date: Time.now + 3.days,
+      driver: users.at(0))
 
 RidesPassenger.create(passenger: users.at(2), ride: ride)
 
 ride = Ride.create(
-    start_city: 'Warszawa',
-    destination_city: 'Kraków',
-    seats: 5,
-    start_date: Time.now + 3.days,
-    driver: users.at(1))
+      start_city: 'Warszawa',
+      destination_city: 'Kraków',
+      seats: 5,
+      start_date: Time.now + 3.days,
+      driver: users.at(1))
 
 RidesPassenger.create(passenger: users.at(3), ride: ride)
 RidesPassenger.create(passenger: users.at(4), ride: ride)
 
 ride = Ride.create(
-    start_city: 'Wrocław',
-    destination_city: 'Poznań',
-    seats: 5,
-    start_date: Time.now + 4.days,
-    driver: users.at(4))
+      start_city: 'Wrocław',
+      destination_city: 'Poznań',
+      seats: 5,
+      start_date: Time.now + 4.days,
+      driver: users.at(4))
 
 RidesPassenger.create(passenger: users.at(1), ride: ride)
 RidesPassenger.create(passenger: users.at(2), ride: ride)
